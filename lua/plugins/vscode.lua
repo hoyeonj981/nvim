bc4a383cfcd45b2fe54600527e52ad2c56cc56c9
hoyeon Jang <hoyeonj981@gmail.com>
2025-09-16 23:42:13 +0900

    feat: add multi cursor

--- lua/plugins/vscode.lua
+++ lua/plugins/vscode.lua
@@ -1,5 +1,19 @@
 return {
 	{
 		import = "lazyvim.plugins.extras.vscode",
-	}
+	},
+
+	{
+		"mg979/vim-visual-multi",
+		branch = "master",
+		init = function()
+			vim.g.VM_maps = {
+				["Find Under"]         = "<C-n>",
+        ["Find Subword Under"] = "<C-n>",
+        ["Add Cursor Down"]    = "<A-Down>",
+        ["Add Cursor Up"]      = "<A-Up>",
+			}
+		vim.g.VM_default_mappings = 0
+		end,
+	},
 }
