fbec332070dccd4edd060734e8ddb6759a1d1deb
hoyeon Jang <hoyeonj981@gmail.com>
2025-09-18 21:37:43 +0900

    feat: configure vim-visual-multi

--- lua/plugins/vscode.lua
+++ lua/plugins/vscode.lua
@@ -1,19 +1,23 @@
 return {
-	{
-		import = "lazyvim.plugins.extras.vscode",
-	},
+  {
+    "mg979/vim-visual-multi",
+    branch = "master",
+    vscode = true,
+    lazy = false,
+    keys = {
+      { "<C-n>", mode = { "n", "x" } },      -- 같은 단어 추가 선택
+      { "<M-j>", mode = { "n", "x" } },      -- 아래로 커서 추가 (Alt/Option+j)
+      { "<M-k>", mode = { "n", "x" } },      -- 위로 커서 추가   (Alt/Option+k)
+    },
 
-	{
-		"mg979/vim-visual-multi",
-		branch = "master",
-		init = function()
-			vim.g.VM_maps = {
-				["Find Under"]         = "<C-n>",
+    init = function()
+      vim.g.VM_default_mappings = 0
+      vim.g.VM_maps = {
+        ["Find Under"]         = "<C-n>",
         ["Find Subword Under"] = "<C-n>",
-        ["Add Cursor Down"]    = "<A-Down>",
-        ["Add Cursor Up"]      = "<A-Up>",
-			}
-		vim.g.VM_default_mappings = 0
-		end,
-	},
+        ["Add Cursor Down"]    = "<M-j>",
+        ["Add Cursor Up"]      = "<M-k>",
+      }
+    end,
+  },
 }
